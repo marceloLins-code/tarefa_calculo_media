3acc40e8535f3eec9c533f0ec2a9c138cfb1e42c
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2023-11-16 23:15:24 -0300

    tarefa_calculo_media

---
+++ src/com/lins/modulo/Programa.java
@@ -0,0 +1,18 @@
+package com.lins.modulo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		double media = 0.0;
+		double num1 = 4.5;
+		double num2 = 5.9;
+		double num3 = 4.5;
+		double num4 = 9.5;
+
+
+		media = num1 + num2 + num3 + num4;
+
+		System.out.println("media = " + media/4 );
+	}
+}
